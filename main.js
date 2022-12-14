8cc7eea4d09b00d9bc8dabdb3bac4d75d487de92
anushka-sing <95973259+anushka-sing@users.noreply.github.com>
2022-12-14 19:30:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ if(results.length>0)
 
 {
     console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
     console.log("nose x="+results[0].pose.nose.x);
     console.log("nose y="+results[0].pose.nose.y);
     
@@ -38,7 +38,7 @@ function draw(){
 image(video,0,0,300,300);
 fill (255,0,0);
 stroke (255,0,0);
-circle(noseX,noseY,20);
+
 image(clown_nose,noseX,noseY,30,30);
 }
 
